20d0939626e4dc4ea896cb779375ceb1057947cd
bojiang <bojiang_@outlook.com>
2024-09-10 17:32:35 +0800

    add bert

---
+++ bert/service.py
@@ -0,0 +1,24 @@
+import bentoml
+from bentoml.models import HuggingFaceModel, BentoModel
+from transformers import AutoModel
+import time
+
+@bentoml.service
+class TestModelPreheat:
+    model = BentoModel("iris_clf:ggthayrc3wqnh4wa")
+    tokenizer = HuggingFaceModel("bert-base-uncased")
+
+    def __init__(self) -> None:
+        start = time.time()
+        print("bento model loaded", self.model)
+        print("huggingface model loaded", self.tokenizer)
+        self.model = AutoModel.from_pretrained(self.tokenizer)
+        print("init time cost", time.time() - start)
+
+    @bentoml.api
+    def generate(self, prompt: str = "hello") -> str:
+        return prompt + " world"
+
+    @bentoml.on_shutdown
+    def on_shutdown(self) -> None:
+        print("on_shutdown")
